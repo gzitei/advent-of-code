fdd2fa935ccb3a5a8c2562d1991b59c08762bd6b
gzitei <gustavo.zitei@gmail.com>
2024-12-02 13:09:15 -0300

    adds part2 test

--- day-02/main_test.go
+++ day-02/main_test.go
@@ -117,12 +117,19 @@ func TestSolvePart1(t *testing.T) {
 	})
 }
 
-// func TestSolvePart2(t *testing.T) {
-// 	test := TestCase{
-// 		"Test Part 2",
-// 		"./part2-test.txt",
-// 		31,
-// 	}
-// 	t.Run(test.name, func(t *testing.T) {
-// 	})
-// }
+func TestSolvePart2(t *testing.T) {
+	test := TestCase{
+		"Test Part 2",
+		"./part2-test.txt",
+		4,
+	}
+	t.Run(test.name, func(t *testing.T) {
+		if sol := SolvePart2(test.file); sol != test.expected {
+			t.Errorf("Test %v: expected %v, got %v",
+				test.name,
+				test.expected,
+				sol,
+			)
+		}
+	})
+}
